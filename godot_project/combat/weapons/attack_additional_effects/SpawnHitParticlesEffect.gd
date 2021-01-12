f8fdb8095cdaa50349a25706655821095f3b09e5
DangerChaser <dangerchasers@gmail.com>
2021-01-11 20:33:12 -0800

    Added common particles, sprites, and sfx
    I reused a lot of assets by duplicating them, and everything got kinda tangly and messed up so I just shoved all art assets into one folder for easier management.

--- godot_project/combat/weapons/attack_additional_effects/SpawnHitParticlesEffect.gd
+++
@@ -1,9 +0,0 @@
-# TODO: Delete, legacy code
-extends AttackAdditionalEffect
-class_name SpawnHitParticlesEffect
-
-
-func enter(args := {}) -> void:
-	.enter(args)
-	var hit_particles = get_parent().get_parent().get_node("HitParticles")
-	hit_particles.spawn(hit_particles.global_position)
